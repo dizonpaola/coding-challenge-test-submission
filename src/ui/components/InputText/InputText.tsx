5b1dbdd2d8c25206133da012127b142ce5142b2a
dizonpaola <paoladizon1996@gmail.com>
2025-08-14 01:42:16 +0800

    Update InputText.tsx

--- src/ui/components/InputText/InputText.tsx
+++ src/ui/components/InputText/InputText.tsx
@@ -2,28 +2,24 @@ import React, { FunctionComponent } from "react";
 
 import $ from "./InputText.module.css";
 
-interface InputTextProps {
+interface InputTextProps extends React.InputHTMLAttributes<HTMLInputElement> {
   name: string;
   placeholder: string;
-  value: string;
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 const InputText: FunctionComponent<InputTextProps> = ({
   name,
-  onChange,
   placeholder,
-  value,
+  ...rest
 }) => {
   return (
     <input
       aria-label={name}
       className={$.inputText}
       name={name}
-      onChange={onChange}
       placeholder={placeholder}
       type="text"
-      value={value}
+      {...rest}
     />
   );
 };
